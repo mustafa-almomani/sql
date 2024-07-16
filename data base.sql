36f500c84b342bcdba1d287360a8830a6424337c
mustafa-almomani <almomani363@gmail.com>
2024-07-16 20:46:29 +0300

    task1 sql 16-7

--- data base.sql
+++ data base.sql
@@ -23,5 +23,60 @@ TRUNCATE TABLE books;
 drop table Authors;
 
 
+create  database school;
+create table Teacher 
+(
+ID int primary key  identity(1,1) ,
+fname varchar(255),
+lname varchar(255),
+);
+
+create table TeacherDetails
+(
+ID int  identity(1,1) ,
+Job_number int,
+birth_day date,
+Specialization varchar(255),
+salary float,
+TeacherID int UNIQUE,
+FOREIGN KEY  (TeacherID)  REFERENCES  Teacher(ID) 
+);
+
+
+create table Courses
+(
+ID int primary key  identity(1,1),
+Courses_name varchar (255),
+TeacherID int ,
+FOREIGN KEY  (TeacherID)  REFERENCES  Teacher(ID) 
+);
+
+
+
+ create table Students 
+ (
+ ID int primary key identity(1,1),
+ Student_name varchar (255),
+ );
+ create table Courses_Students
+ (
+CoursesID int ,
+ StudentID int ,
+FOREIGN KEY  (StudentID)  REFERENCES Students(ID),
+FOREIGN KEY  (CoursesID)  REFERENCES Courses(ID)
+
+ );
+
+
+ 
+ select * from Courses;
+ select * from Students;
+ select * from Teacher;
+ select * from TeacherDetails;
+
+
+
+
+
 
 
